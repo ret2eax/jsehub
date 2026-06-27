48106579a3fc26f869c158f19d31f1a323d2cb4d
ret2eax <ret2eax@users.noreply.github.com>
2026-06-27 05:56:02 +0000

    deploy: 0bf7343f8f362fc8385118d634ec4ea655374370

---
+++ _next/static/8QhAuTZoYQPgv4bcO78jV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/245-4bc9fabb8dc6c274.js","static/chunks/pages/index-6c95a6cd1d6b158e.js"],"/_error":["static/chunks/pages/_error-922864f821a8940a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
