1799172c81225accdf7df1d336b6fecd23f1a442
ret2eax <ret2eax@users.noreply.github.com>
2026-06-27 05:07:47 +0000

    deploy: beeabea3b32c8c91ce3e4fde7a39f8ca89129015

---
+++ _next/static/iJ1244i65fZeu5PHAizui/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/245-4bc9fabb8dc6c274.js","static/chunks/pages/index-e4d919ea7eb48145.js"],"/_error":["static/chunks/pages/_error-922864f821a8940a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
