b674548a35fd5f5b982400d7deed49a1c0e72cc9
ret2eax <ret2eax@users.noreply.github.com>
2026-06-27 03:18:29 +0000

    deploy: 856bf898caa8f2ac7601ecea667549a5220345d4

--- _next/static/5RKfJ-wwo2hqbWvgJOI-7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-01b1f105beb3a7f9.js"],"/_error":["static/chunks/pages/_error-922864f821a8940a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
