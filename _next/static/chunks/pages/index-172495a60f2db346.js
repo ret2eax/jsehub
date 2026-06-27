93f8e6bcda6f5643555cb68523d1db80b5d1b919
ret2eax <ret2eax@users.noreply.github.com>
2026-06-27 13:10:28 +0000

    deploy: 8f7eeb23b0b759c07a012bb15d754ffce7935d76

---
+++ _next/static/chunks/pages/index-172495a60f2db346.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{3707:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(6957)}])}},function(n){n.O(0,[944,957,888,774,179],function(){return n(n.s=3707)}),_N_E=n.O()}]);
--- _next/static/f8lCjUq5nHn5Odrmqfo3U/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/245-4bc9fabb8dc6c274.js","static/chunks/pages/index-0a408356f54a1cb7.js"],"/_error":["static/chunks/pages/_error-922864f821a8940a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
